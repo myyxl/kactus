9979e6d47c2b4a209bd4362c000e8116945e95fa
myxl <22593897+myyxl@users.noreply.github.com>
2021-08-12 08:38:31 +0200

    reverted changes of client packet handler (good for now)

--- src/main/kotlin/network/packet/ClientPacketHandler.kt
+++ src/main/kotlin/network/packet/ClientPacketHandler.kt
@@ -7,41 +7,30 @@ import network.packet.client.login.CLoginStartPacket
 import network.packet.client.play.CPlayKeepAlivePacket
 import network.packet.client.status.CStatusPingPacket
 import network.packet.client.status.CStatusRequestPacket
-import network.stream.PacketInputStream
 
 
 class ClientPacketHandler {
 
-    private val packetList = listOf(
-        CHandshakePacket::class,
-        CStatusRequestPacket::class,
-        CStatusPingPacket::class,
-        CLoginStartPacket::class,
-        CPlayKeepAlivePacket::class
-    )
-
     @Synchronized fun handle(packetId: Int, packetBuffer: ByteArray, clientConnection: ClientConnection) {
-        /*
-                Not working because buffer is given and
-                every packet tries to read from it and fails
-                if not the correct packet is on top of the list.
-                Because the (wrong) packet expects a datatype which is not in
-                the given buffer because it wasn't intended for the
-                specific packet. Thus, it will hang and wait for the value.
-         */
+        when(clientConnection.clientState) {
 
-        packetList.forEach {
-            println("handle() loop: ${it.simpleName}")
-
-            val packet = it.java.getConstructor(ClientConnection::class.java,
-                            PacketInputStream::class.java).newInstance(clientConnection, packetBuffer)
-
-            if(clientConnection.clientState == packet.state && packetId == packet.packetId) {
-                packet.onReceive()
-                print("Object state: " + packet.state)
-                return
+            ClientState.HANDSHAKE -> when(packetId) {
+                0x00 -> CHandshakePacket(clientConnection, packetBuffer).onReceive()
             }
+
+            ClientState.STATUS -> when(packetId) {
+                0x00 -> CStatusRequestPacket(clientConnection, packetBuffer).onReceive()
+                0x01 -> CStatusPingPacket(clientConnection, packetBuffer).onReceive()
+            }
+
+            ClientState.LOGIN -> when(packetId) {
+                0x00 -> CLoginStartPacket(clientConnection, packetBuffer).onReceive()
+            }
+
+            ClientState.PLAY -> when(packetId) {
+                0x0F -> CPlayKeepAlivePacket(clientConnection, packetBuffer).onReceive()
+            }
+
         }
-        println("test")
     }
 }
