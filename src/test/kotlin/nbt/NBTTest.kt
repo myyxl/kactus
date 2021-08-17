aa951a7f01c4c8452ad167e637092c19e4b53fd3
myxl <22593897+myyxl@users.noreply.github.com>
2021-08-17 19:55:26 +0200

    improved NBT test cases

--- src/test/kotlin/nbt/NBTTest.kt
+++ src/test/kotlin/nbt/NBTTest.kt
@@ -8,14 +8,8 @@ import kotlin.test.assertEquals
 internal class NBTTest {
 
     @Test
-    fun readFromFile() {
-        var compoundTag = NBT().readFromFile("src/test/resources/small_uncompressed.nbt")
-        val shortValue = (compoundTag.getTag("ImShort") as TagShort).value
-        val stringTag = (compoundTag.getTag("name") as TagString).value
-        assertEquals(shortValue, (1337).toShort())
-        assertEquals(stringTag, "Testing")
-
-        compoundTag = NBT(NBTCompression.GZIP).readFromFile("src/test/resources/small_gzip_compressed.nbt")
+    fun testDecompressionFromFile() {
+        val compoundTag = NBT(NBTCompression.GZIP).readFromFile("src/test/resources/test-compression.nbt")
         val byteTag = (compoundTag.getTag("OnGround") as TagByte).value
         val listTag = (compoundTag.getTag("Pos") as TagList<*>)
         val doubleTag = (listTag.getList()[1]).value
@@ -23,11 +17,68 @@ internal class NBTTest {
         assertEquals(doubleTag, NaN)
     }
 
-    /*
-        Testing the most important tags, if most of the primitives work, all of them should work...
-     */
     @Test
-    fun deserialize() {
+    fun testDeserializeFromFile() {
+        val mainTag = NBT().readFromFile("src/test/resources/test-deserialize.nbt")
+
+        assertEquals((7).toByte(), (mainTag.getTag("ByteTagFull") as TagByte).value)
+        assertEquals((0).toByte(), (mainTag.getTag("ByteTagNoValue") as TagByte).value)
+
+        assertEquals((1337).toShort(), (mainTag.getTag("ShortTagFull") as TagShort).value)
+        assertEquals((0).toShort(), (mainTag.getTag("ShortTagNoValue") as TagShort).value)
+
+        assertEquals(1234567890, (mainTag.getTag("IntTagFull") as TagInt).value)
+        assertEquals(0, (mainTag.getTag("IntTagNoValue") as TagInt).value)
+
+        assertEquals((1234567897654321).toLong(), (mainTag.getTag("LongTagFull") as TagLong).value)
+        assertEquals((0).toLong(), (mainTag.getTag("LongTagNoValue") as TagLong).value)
+
+        assertEquals((1.2).toDouble(), (mainTag.getTag("DoubleTagFull") as TagDouble).value)
+        assertEquals((0).toDouble(), (mainTag.getTag("DoubleTagNoValue") as TagDouble).value)
+        assertEquals(NaN, (mainTag.getTag("DoubleTagNaN") as TagDouble).value)
+
+        assertEquals((3.141592).toFloat(), (mainTag.getTag("FloatTagFull") as TagFloat).value)
+        assertEquals((0).toFloat(), (mainTag.getTag("FloatTagNoValue") as TagFloat).value)
+        assertEquals((NaN).toFloat(), (mainTag.getTag("FloatTagNaN") as TagFloat).value)
+
+        assertEquals("Hello World!", (mainTag.getTag("StringTagFull") as TagString).value)
+        assertEquals("", (mainTag.getTag("StringTagNoValue") as TagString).value)
+        assertEquals("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+            (mainTag.getTag("StringTagLong") as TagString).value)
+        assertEquals("!\"§\$%&/()=?@²³{}[]\\+*#'_-.:,;µ<>öäü", (mainTag.getTag("StringTagSpecial") as TagString).value)
+
+        assert(byteArrayOf(-34, -83, -66, -17).contentEquals(((mainTag.getTag("ByteArrayTag") as TagByteArray).value) as ByteArray))
+
+        assert(intArrayOf(286331153, 572662306, 858993459, 1145324612, 1431655765
+        ).contentEquals(((mainTag.getTag("IntArrayTag") as TagIntArray).value) as IntArray))
+
+        assert(longArrayOf(1229782938247303441, 2459565876494606882, 3689348814741910323
+        ).contentEquals(((mainTag.getTag("LongArrayTag") as TagLongArray).value) as LongArray))
+
+        assertEquals(0, ((mainTag.getTag("TagCompoundEmpty") as TagCompound).getTags().size))
+
+        assertEquals(21, ((mainTag.getTag("TagCompoundFull") as TagCompound).getTags().size))
+
+        assertEquals(2, ((mainTag.getTag("TagCompoundNested") as TagCompound).getTags().size))
+
+        val nestedChild1 = ((mainTag.getTag("TagCompoundNested") as TagCompound)).getTag("TagCompoundNestedChild1") as TagCompound
+        val nestedChild2 = ((mainTag.getTag("TagCompoundNested") as TagCompound)).getTag("TagCompoundNestedChild2") as TagCompound
+        assertEquals(21, nestedChild1.getTags().size)
+        assertEquals(21, nestedChild2.getTags().size)
+
+        assertEquals(0, (mainTag.getTag("ListTagEmpty") as TagList<*>).getList().size)
+
+        assertEquals(4, (mainTag.getTag("ListTagByte") as TagList<*>).getList().size)
+        assertEquals(4, (mainTag.getTag("ListTagShort") as TagList<*>).getList().size)
+        assertEquals(2, (mainTag.getTag("ListTagInt") as TagList<*>).getList().size)
+        assertEquals(2, (mainTag.getTag("ListTagLong") as TagList<*>).getList().size)
+        assertEquals(4, (mainTag.getTag("ListTagDouble") as TagList<*>).getList().size)
+        assertEquals(3, (mainTag.getTag("ListTagFloat") as TagList<*>).getList().size)
+        assertEquals(3, (mainTag.getTag("ListTagString") as TagList<*>).getList().size)
+    }
+
+    @Test
+    fun testSerialize() {
         var tag: Tag = TagString("tagName123", "tagValue345")
         var serialized = NBT().serialize(tag)
         var deserialized = NBT().deserialize(serialized)
